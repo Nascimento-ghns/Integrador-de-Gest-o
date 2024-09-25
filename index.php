ba1fd84bb270eaf4b989a6053779f59387f3c6e9
Nascimento-ghns <gn29655@gmail.com>
2024-09-25 08:45:07 -0300

    inclusão de bibliotecas bootstrap, criação da pagina de login e do arquivo de estilização css

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="bibliotecas/css/bootstrap.css">
+    <link rel="stylesheet" href="bibliotecas/js/bootstrap.js">
+    <link rel="stylesheet" href="css/estilo.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Integrador de Gestão</title>
+</head>
+<body>
+    <section class="container">
+        <div  class="row">
+            <div class="col-md-4"></div>    
+            <div class="col-md-4">
+                <form action="">
+                    <h1>Login</h1>
+                    <hr>
+                    <label for="email">Email:</label>
+                    <input type="text" placeholder="digite seu email">
+                    <br>
+                    <label for="senha">Senha:</label>
+                    <input type="text" placeholder="digite sua senha">
+                </form>
+            </div>
+        </div>    
+    </section>
+</body>
+</html>
